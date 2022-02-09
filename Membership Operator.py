e7ad6a38a868f75a7cc11c57c343cd5a0e07d016
DhavalSach <99311899+DhavalSach@users.noreply.github.com>
2022-02-09 11:32:06 +0530

    Add files via upload

---
+++ Membership Operator.py
@@ -0,0 +1,9 @@
+# membership operator
+'''it is used to check whether the element we are trying to find is available or not'''
+
+basket = {
+    "apple", "eggs", "banana", "orange"
+}
+
+print("eggs" in basket)
+print("chicken" in basket)
